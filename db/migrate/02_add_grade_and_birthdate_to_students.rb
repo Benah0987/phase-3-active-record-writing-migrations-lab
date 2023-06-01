e4fa4489b9c6bfb77581b083f3bfa85d04777269
Benah0987 <wanyoikebenayah77@gmail.com>
2023-06-01 21:56:10 +0300

    doing migrations, adding and changing columns,

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[6.0]
     def change
+        # used add_column
+        # it takes three arguments
       add_column :students, :grade, :integer
       add_column :students, :birthdate, :string
     end
